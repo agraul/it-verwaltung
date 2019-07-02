7b7b121dde4918fb7402c6f9171eda43297ceb2d
Chris <chrisi468@gmx.de>
2019-07-02 16:06:51 +0200

    feat(roomDetailed): add display of software and Hardware

---
+++ frontend/src/app/fe-entities/component.entity.ts
@@ -0,0 +1,8 @@
+export class HComponent {
+    id: number;
+    bezeichnung: string;
+    seriennummer: string;
+    hersteller: string;
+    is_software: boolean;
+
+}
--- frontend/src/app/room/room-card/room-card.component.ts
+++ frontend/src/app/room/room-card/room-card.component.ts
@@ -12,6 +12,8 @@ export class RoomCardComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    
+    
   }
 
   
