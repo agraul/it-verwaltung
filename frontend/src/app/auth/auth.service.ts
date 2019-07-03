79cdfc827a348df17bd80cca7fde3ddd1900ed91
Lukas Nürnberger <l.nuernberger@online.de>
2019-07-03 19:54:13 +0200

    formatting

--- frontend/src/app/auth/auth.service.ts
+++ frontend/src/app/auth/auth.service.ts
@@ -5,7 +5,7 @@ import { tap, delay } from 'rxjs/operators';
 import { USERS } from '../log-in/log-in.mock';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class AuthService {
   isLoggedIn = false;
@@ -14,15 +14,14 @@ export class AuthService {
   // store the URL so we can redirect after logging in
   redirectUrl: string;
 
-  login(username: string ,password: string ): Observable<boolean> {
-      if(this.getToken(username, password)){
-              return of(true).pipe(
-             delay(1000),
-             tap(val => this.isLoggedIn = true)
-             );
-      }else{ 
-    return of(false).pipe(
-    )
+  login(username: string, password: string): Observable<boolean> {
+    if (this.getToken(username, password)) {
+      return of(true).pipe(
+        delay(100),
+        tap(val => (this.isLoggedIn = true))
+      );
+    } else {
+      return of(false).pipe();
     }
   }
 
@@ -31,7 +30,7 @@ export class AuthService {
   }
 
   getToken(usr: string, pwd: string) {
-      //TODO Api Call
-      return true;
+    // TODO: Api Call
+    return true;
   }
 }
--- frontend/src/app/log-in/log-in.component.ts
+++ frontend/src/app/log-in/log-in.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {  Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { USERS } from './log-in.mock';
 import { AuthService } from '../auth/auth.service';
 
@@ -9,37 +9,38 @@ import { AuthService } from '../auth/auth.service';
   styleUrls: ['./log-in.component.less']
 })
 export class LogInComponent implements OnInit {
-
   users = USERS;
   userName = '';
   passWord = '';
-  showValidationMessage= false;
+  showValidationMessage = false;
 
-  constructor(
-    private router: Router,
-    private authentication: AuthService,
-  ) { }
+  constructor(private router: Router, private authentication: AuthService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  
-  public secureAndSend(username: string, password: string): void{
-   console.log(this.authentication.login(username, password).subscribe((resp)=> {return resp;}));
-    
+  public secureAndSend(username: string, password: string): void {
+    console.log(
+      this.authentication.login(username, password).subscribe(resp => {
+        return resp;
+      })
+    );
+
     let retVal = false;
-    console.log(this.authentication.login(username, password).subscribe((resp)=> {return resp;}).closed === false);
-    
-          if(this.authentication.login(username, password).subscribe((resp)=> {
-            this.router.navigate(['/rooms']);
-            return resp;
-          }).closed === false){
-            
-            retVal = false;
+    console.log(
+      this.authentication.login(username, password).subscribe(resp => {
+        return resp;
+      }).closed === false
+    );
 
-          }
-        else{
-          this.showValidationMessage = true;
-        }
+    if (
+      this.authentication.login(username, password).subscribe(resp => {
+        this.router.navigate(['/rooms']);
+        return resp;
+      }).closed === false
+    ) {
+      retVal = false;
+    } else {
+      this.showValidationMessage = true;
+    }
   }
-  }
+}
