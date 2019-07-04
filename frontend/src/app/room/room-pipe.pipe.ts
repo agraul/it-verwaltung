82e9c0f6f60579dae9d1dec357aa8b06ee78de0f
asdfasdf <graffittipfilda@gmail.com>
2019-07-04 14:12:40 +0200

    added room pipe

---
+++ frontend/src/app/room/room-pipe.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Room } from '../fe-entities/room.entity';
+import { Card } from '../fe-entities/card.entity';
+
+@Pipe({
+  name: 'roomPipe'
+})
+export class RoomPipePipe implements PipeTransform {
+
+  transform(allRooms: Card[], search: string): any {
+    if (!allRooms || !search) {
+      return allRooms;
+  }
+    console.log(allRooms[1].header);
+    console.log(allRooms.filter(room =>{return room}));
+    
+    return allRooms.filter(room => room.header   === search);
+  }
+
+}
